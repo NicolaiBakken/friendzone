1d891213ef8e257397cf02f81102d49c542628e4
NicolaiBakken <31101110+NicolaiBakken@users.noreply.github.com>
2018-12-11 12:50:38 +0100

    Add files via upload

--- scripts/chatfunction.js
+++ scripts/chatfunction.js
@@ -1,1 +1,57 @@
 /* Creates the chat for FriendZone */
+
+//HTML objects
+const chatSection = document.getElementById("chat-container");
+const sendButton = document.getElementById("send-button");
+
+//Forskjellige arrays med informasjon om de forskjellige artiklene som genereres ferdig utfylt.
+let inputText = document.getElementById("text-input");
+let messageContainer = document.getElementById("message-container");
+let sentMessage = "";
+
+sendButton.onclick = sendMessage;
+
+
+//genererer chatten med meldinger
+function sendMessage(){
+    
+    
+    if(inputText.value != null) {
+    alert(inputText.value);
+    let message = "";
+    
+    
+        message += `
+            <div id="messageArea">
+                <p id="sentMessage">${inputText.value}</p>
+            </div>
+        `;
+    
+    sentMessage = inputText.value;    
+    inputText.value = "";
+    
+        
+    messageContainer.innerHTML += message;
+        
+    autoReply();
+               
+    }
+}
+
+function autoReply(){
+    if(sentMessage == "Hei") {
+        let replyMessage = "";
+        
+        replyMessage += `
+            <div id="messageArea-reply">
+                <p id="sentMessage">Halla!</p>
+            </div>
+        `;
+        alert("SHIT!");
+        messageContainer.innerHTML += replyMessage;   
+        
+    }
+        
+        
+    
+}
